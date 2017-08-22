3b0d037302c64fcc8c9abb270291a15212e298e3
Baldon <Baldon@DESKTOP-S5N21GO>
2017-08-22 11:40:38 +0200

    fine del proetto

--- src/esercitazioni/polito/libretto/LibrettoController.java
+++ src/esercitazioni/polito/libretto/LibrettoController.java
@@ -1,5 +1,113 @@
 package esercitazioni.polito.libretto;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import esercitazioni.polito.libretto.model.Esame;
+import esercitazioni.polito.libretto.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class LibrettoController {
+	/*
+	 * se il programa fosse più complesso ci sarebbero più viste e quindi ci sarebbero più 
+	 * controllore ma il modello deve essere unico quinid non  può ogni controllore 
+	 * crearsi un modello diverso
+	 */
+	//Model model = new Model();
+	// è un riferimento al modello dato dal main
+	Model model;
 	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtxCodice;
+
+    @FXML
+    private TextField txtTitolo;
+
+    @FXML
+    private TextField txtDocente;
+
+    @FXML
+    private Button btnCerca;
+
+    @FXML
+    private Button btnInserisci;
+
+    @FXML
+    private TextArea txtMessage;
+
+    @FXML
+    void handleCerca(ActionEvent event) {
+    	String codice = txtxCodice.getText();
+    	
+    	if(codice.length()<5) {
+    		txtMessage.appendText("codice non valido\n");
+    		return;
+    	}
+    	
+    	Esame e = model.trovaEsame(codice);
+    	
+    	if (e==null) {
+    		txtMessage.appendText("codice "+codice+ " non trovato\n");
+    	}else {
+    		txtMessage.appendText("codice "+codice+ " trovato\n");
+    		
+    		txtxCodice.setText(e.getCodice());
+    		txtTitolo.setText(e.getTitolo());
+    		txtDocente.setText(e.getDocente());
+    	}
+    }
+
+    @FXML
+    void handleInserisci(ActionEvent event) {
+    	// recupera i dati dall'interfaccia
+    	String codice = txtxCodice.getText();
+    	String titolo = txtTitolo.getText();
+    	String docente = txtDocente.getText();
+    	
+    	//verifica la validità dei dati, non può mai essere null 
+    	if(codice.length() < 5 || titolo.length() == 0 || docente.length() == 0) {
+    		txtMessage.appendText("dati non corretti\n");
+    		return;
+    	}
+    	
+    	//chiede al model di effettuare l'operazione, in questo caso l'inserimento
+    	Esame e = new Esame(codice, titolo, docente);
+    	boolean resutl = model.addCorso(e);
+    	
+    	//aggiorna la vsta con il risultato dell'operazione
+    	if(resutl) {
+    		txtMessage.appendText("Esame aggiunto correttamente\n");
+    	}else {
+    		txtMessage.appendText("Esame NON AGGIUNTO perchè già presente un esame con lo stesso codice\n");
+    	}
+    	
+    }
+
+    @FXML
+    void initialize() {
+        assert txtxCodice != null : "fx:id=\"txtxCodice\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtTitolo != null : "fx:id=\"txtTitolo\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtDocente != null : "fx:id=\"txtDocente\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btnCerca != null : "fx:id=\"btnCerca\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Libretto.fxml'.";
+        assert txtMessage != null : "fx:id=\"txtMessage\" was not injected: check your FXML file 'Libretto.fxml'.";
+
+    }
+
+	/**
+	 * @param model the model to set
+	 */
+	public void setModel(Model model) {
+		this.model = model;
+	}
 }
--- src/esercitazioni/polito/libretto/Main.java
+++ src/esercitazioni/polito/libretto/Main.java
@@ -1,5 +1,6 @@
 package esercitazioni.polito.libretto;
 	
+import esercitazioni.polito.libretto.model.Model;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,8 +12,25 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Libretto.fxml"));
-			Scene scene = new Scene(root,400,400);
+			/*
+			 * il main generato automaticamente utilizza un metodo statico che non lascia tracciaù
+			 * dell'oggetto. quindi una volta che ho creato il loader il programma se ne dimentica 
+			 * immadiatamente. per tenere traccia del loader devo crearlo espicitamente spezzando 
+			 * in due il comando statico
+			 */
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Libretto.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			//fxmlloader deve dirmi chi è il controller
+			
+			/*
+			 * lemodifiche chefacciamo sono douvute al fatto che il main auto generato
+			 * non è pensato per avere un modello separato
+			 */
+			LibrettoController controller = loader.getController();
+			Model model = new Model();
+			controller.setModel(model);
+			
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
